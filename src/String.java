78c1e2bc2ad73fb56308adc53dc295057c1e2ea6
Mansiiiiiiiiiiiiiiiii <mansi15094@gmail.com>
2024-10-04 18:18:50 +0530

    Java

---
+++ src/String.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class String {
+public static void main(java.lang.String args[]){
+    Scanner sc= new Scanner(System.in);
+    java.lang.String name = "Mansi";
+    System.out.println("Your name is"+name);
+}
+}
